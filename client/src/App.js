5ba83188974bd9a241f1d7af8e6f45dc2e0431dc
EssWhyy <sventang1@gmail.com>
2022-08-18 15:00:15 +0800

    Added Basic Table with CRUD Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,64 @@
-import React, { Component }  from 'react';
-import logo from './logo.svg';
+import { CompletionListener } from 'concurrently/dist/src/completion-listener';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import ButtonAppBar from './components/navbar.js'
+import ButtonAppBar from './components/navbar.js';
+import EnhancedTable from './components/table.js';
 
 function App() {
+
+  //Table Data Section
+  const currentRows = [
+    {name:'Cupcake', modcode:305, roundzero:3.7, roundone:67, roundtwo:4.3},
+    {name:'Donut', modcode:452, roundzero:25.0, roundone:51, roundtwo:4.9},
+    {name:'Eclair', modcode:262, roundzero:16.0, roundone:24, roundtwo:6.0}
+  ];
+  
+  const [tableRows, setTableRows] = useState(currentRows)
+  const [modregSummary, getModRegSummary] = useState("")
+
+  //adds row from search bar, if not already added
+  const addRow = () => {
+    let addedRow = {name:'a', modcode:1, roundzero:1, roundone:1, roundtwo:1}
+    let newArray = tableRows.slice()
+    let duplicate = false
+
+    for (let i = 0; i<newArray.length; i++){
+      if (newArray[i].name == addedRow.name){
+        duplicate = true
+        break
+      }
+    }
+
+    if (!duplicate){ 
+      newArray.push(addedRow)
+      setTableRows(newArray)
+    }
+  }
+
+
+  //gets module summary 
+  useEffect(() => {
+    // Update the document title using the browser API
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => getModRegSummary(data.numberOfMods))
+  });
+
+
   return (
     <div className="App">
       <ButtonAppBar></ButtonAppBar>
       <header className="App-header">     
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <button onClick={addRow}>Add a Module</button>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Check Module Demand for NUS Modules from ModReg Round 0-3
+          Current Semester: AY22/23 Sem 1
+          Number of Mods: {modregSummary}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <EnhancedTable inputRows={tableRows} delRows={setTableRows}></EnhancedTable>
       </header>
     </div>
   );
---
+++ client/src/components/table.js
@@ -0,0 +1,305 @@
+import * as React from 'react';
+import {useState, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Paper from '@mui/material/Paper';
+import CloseIcon from '@mui/icons-material/Close';
+import { visuallyHidden } from '@mui/utils';
+
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+const headCells = [
+  {
+    id: 'name',
+    numeric: false,
+    disablePadding: true,
+    label: 'Module Name',
+  },
+  {
+    id: 'modcode',
+    numeric: true,
+    disablePadding: false,
+    label: 'Module Code',
+  },
+  {
+    id: 'roundzero',
+    numeric: true,
+    disablePadding: false,
+    label: 'Round Zero (demand/slots)',
+  },
+  {
+    id: 'roundone',
+    numeric: true,
+    disablePadding: false,
+    label: 'Round One (demand/slots)',
+  },
+  {
+    id: 'roundtwo',
+    numeric: true,
+    disablePadding: false,
+    label: 'Round Two (demand/slots)',
+  },
+];
+
+//Below section should not be edited
+
+function EnhancedTableHead(props) {
+  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+    props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const EnhancedTableToolbar = (props) => {
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+        }),
+      }}
+    >
+
+    </Toolbar>
+  );
+};
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
+
+//Above section should not be edited
+
+
+export default function EnhancedTable({inputRows, delRows}) {
+
+  const [rows, setRows] = useState(inputRows);
+
+  useEffect(() => {
+    setRows(inputRows);
+    console.log('useEffect logic ran');
+  }, [inputRows]);
+
+
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('modcode');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size='medium'
+          >
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {Array.from(rows).slice().sort(getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const labelId = row.name;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.name)}
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.name}
+                    >
+                      <TableCell padding="checkbox">
+                        
+                        <CloseIcon onClick={() => {
+                          var newArray = inputRows.slice()
+                          var index = -1
+                          for (var i = 0; i < newArray.length; i++) {
+                            if (newArray[i].name == labelId){
+                              index = i
+                              break
+                            }
+                          }
+
+                          if (index > -1) {
+                            newArray.splice(index, 1)
+                            delRows(newArray)
+                          }
+
+                        }}></CloseIcon>
+                      </TableCell>
+                      
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="right">{row.modcode}</TableCell>
+                      <TableCell align="right">{row.roundzero}</TableCell>
+                      <TableCell align="right">{row.roundone}</TableCell>
+                      <TableCell align="right">{row.roundtwo}</TableCell>
+                    </TableRow>
+                  );
+                })}
+
+              {}
+
+
+
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+}
